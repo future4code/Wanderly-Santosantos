f7c09319499c515e7e7aa88851810bba7f05f76d
wanderly00 <wanderly.francisco15@gmail.com>
2021-08-11 13:29:06 -0300

    comentario

---
+++ semana1/Variaveis/index.js
@@ -0,0 +1,63 @@
+//1. Analise o programa abaixo e diga o que será impresso no console, **SEM EXECUTAR o programa.**
+
+   /* ```jsx
+    let a = 10
+    let b = 10
+
+    console.log(b)
+
+    b = 5
+    console.log(a, b)
+    ```
+    //Resposta: vai ser impresso primeiramente 10 e depois 5,10
+
+    2. Analise o programa abaixo e diga o que será impresso no console, **SEM EXECUTAR o programa.**
+
+    ```jsx
+    let a = 10
+    let b = 20
+    c = a
+    b = c
+    a = b
+
+    console.log(a, b, c)
+    ```
+        Resposta: sera impreso 10,20,10
+
+
+3. Analise o programa abaixo, entenda o que ele faz e **sugira melhores nomes para as variáveis**. Lembre-se que devemos escolher nomes significativos, usar o padrão **camelCase**. Alem disso, os nomes não podem começar com números ou caracteres especiais.
+
+    ```jsx
+    let p = prompt("Quantas horas você trabalha por dia?")
+    let t = prompt("Quanto você recebe por dia?")
+    alert(`Voce recebe ${t/p} por hora`)
+    ```
+
+    Resposta:   let horas-dia
+                let valor-dia
+                alert (horas-dia/valor-dia)
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------]
+/***
+*/
+//const nome  = ("")
+//console.log (typeof nome)
+
+//const nome = prompt ("Qual e o seu nome?")
+//console.log (typeof nome)
+
+//const idade = prompt ("Qual e a sua idade?")
+//console.log (typeof idade)
+
+//console.log = ("Ola wendy, voce tem 20 anos")
+
+//const question = ("Esta chovendo hoje?")
+//console.log = (question)
+
+//const question2 = ("esta frio?")
+//console.log = (question2)
+
+//const question3 = ("como foi a noite?")
+//console.log = (question3)
+
