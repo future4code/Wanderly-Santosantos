79aadc9f86a1e07b9c29fc49d1a55c9bb90b401a
Wendy Bonnie <86128063+Wanderly00@users.noreply.github.com>
2021-08-12 15:01:00 -0300

    Add files via upload

---
+++ semana1/index.js
@@ -0,0 +1,37 @@
+/**Respostas atividade semana1- aula04
+ * Resposta 1: a:false, b:false, c:true d:boolean
+ * resposta 2: O problema e que ele nao declarou a variavel correta. Sera impresso no terminal um erro
+ * resposta 3:let numero1 = +prompt("Digite um numero")
+let numero2 = +prompt ("Digite um numero")
+
+alert(numero1 + numero2)
+ * 
+ * */
+
+// Exercicio de digitação de codigo
+//1
+//let idade = prompt("Qual a sua idade?");
+//let idademigo = prompt("Qual a idade do seu melhor amigo?");
+//const condiçao = idade === idademigo;
+//console.log(condiçao);*/
+
+//2.
+//let numeropar = prompt("Insira um numero par!")
+//let numero2 = 2
+//let resto = numeropar / numero2
+//console.log(resto);
+// sim e possivel ve um padrao, e se o usario inserir um numero impar ele dividira por dois tambem e dara resultado em numero quebrado
+
+//3
+//let idade = prompt("Quantos anos você tem?")
+//let meses = 12
+//let dias = 365
+//let horas = 24
+//let resultado = idade * meses * dias * horas
+//console.log(resultado)
+
+//4
+//let numeros = prompt("Insira um numero")
+//let numeros2 = prompt("insira um numero")
+//let resultados = numeros > numeros2
+//console.log (resultados)
