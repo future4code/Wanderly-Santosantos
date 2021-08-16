7a0f97edf89e6cc811fe7c8ae500de9b56aabc36
wanderly00 <wanderly.francisco15@gmail.com>
2021-08-16 16:06:19 -0300

    comentario

---
+++ semana2/semana2-aula1/index.js
@@ -0,0 +1,47 @@
+//       RESPOSTAS DAS ATIVIDADES SOBRE OBJETOS 
+
+//1. a: Vai ser impresso no console Mattheus Nachtergaele, Virginia Cavendish, canal:globo, Horario: 14h
+
+//2. a: Vai ser impresso no console nome: Juca, Idade:3 raça:srd 3vzs --- B: a sintaxe de tres pontos nao ira fazer nada
+
+//3. a: Vai ser impresso false, undefined --- b:o valor impresso no console retornou undefined pois nao foi declarada a altura.
+
+// ------- EXERCICIOS DE ESCRITA DE CODIGO ---------
+
+// 1.  
+//const pessoa = {
+//        nome: "Fernanda"    
+//     apelidos: ["Nanda", "Nandinha", "Fefe"]
+//}
+//console.log (pessoa)
+
+//2.
+//const nome1 = {
+//    nome: "Marcelo",
+//    idade:"30",
+//    profissao:"Medico"
+//}
+//console.log (nome1)
+
+//3.
+
+//a: 
+//const carrinho = {
+
+//b
+//    fruta1 : {
+//    nome: "Maça",
+//    disponibilidade: true
+//},
+//    fruta2 : {
+//    nome:"Laranja",
+//    disponibilidade: true 
+//},
+//    fruta3 : {
+//    nome: "Morango",
+//    disponibilidade: true
+//}
+//}
+//c.
+//carrinho.push(fruta1,fruta2,fruta3)
+//console.log(carrinho)
